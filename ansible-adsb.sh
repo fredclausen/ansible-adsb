8e85bdd21c6bb438ddc89e0e58e5980bb5aa1ed6
fredclausen <clausen.fred@icloud.com>
2020-11-30 21:24:06 -0700

    progress

--- ansible-adsb.sh
+++ ansible-adsb.sh
@@ -1,40 +1,51 @@
 #!/bin/bash
 
+MASTER_IP="192.168.31.30"
+USER="ubuntu"
+
 echo "Welcome to server setup and maintence"
 
 while true
 do
-	echo "Select 0) To provision servers"
-	echo "Select 1) For Rancher"
-	echo "Select 3) For k3s"
-	echo "Select 4) To delete existing rancher cluster"
-	echo "Select 5) To delete existing k3s cluster"
-	echo "Select 6) To reboot nut server"
-	echo "Select 7) To update servers"
-	echo "Select 8) To exit"
+	echo "Select a) To provision servers"
+	echo "Select b) To install Rancher"
+	echo "Select c) To install Rancher cluster to the nodes"
+	echo "Select d) To install k3s"
+	echo "Select e) To delete existing rancher cluster"
+	echo "Select f) To delete existing k3s cluster"
+	echo "Select g) To reboot nut server"
+	echo "Select h) To update servers"
+	echo "Select i) To setup cluster pods"
+	echo "Select j) To remove cluster pods"
+	echo "Select k) To exit"
 	read -p "Enter selection " answer
 
 	case $answer in
-		[0]* ) echo "Provisioning servers"
+		[aA]* ) echo "Provisioning servers"
 		       ansible-playbook -i inventory/inventory --extra-vars "@group_vars/all.yaml" server/server-setup.yaml;;
-		[1]* ) echo "Setting up rancher"
+		[bB]* ) echo "Setting up rancher"
 		       ansible-playbook -i inventory/inventory --extra-vars "@group_vars/all.yaml" server/rancher-deploy.yaml
 		       echo "Remember to log in to the rancher web interface and update group_vars/all.yaml before proceeding";;
-		[2]* ) echo "Setting up rancher cluster"
+		[cC]* ) echo "Setting up rancher cluster"
                ansible-playbook -i inventory/inventory --extra-vars "@group_vars/all.yaml" server/rancher-node-setup.yaml;;
-		[3]* ) echo "Setting up k3s cluster"
-		       ansible-playbook -i inventory/inventory --extra-vars "@group_vars/all.yaml" k3s/site.yaml;;
-		[4]* ) echo "Deleting rancher cluster"
+		[dD]* ) echo "Setting up k3s cluster"
+		       ansible-playbook -i inventory/inventory --extra-vars "@group_vars/all.yaml" k3s/site.yml
+		       rm -f ~/.kube/config
+		       scp $USER@$MASTER_IP:~/.kube/config ~/.kube/config
+		       kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml;;
+		[eE]* ) echo "Deleting rancher cluster"
                ansible-playbook -i inventory/inventory --extra-vars "@group_vars/all.yaml" server/rancher-remove.yaml;;
-        [5]* ) echo "Deleting k3s cluster"
-               ansible-playbook -i inventory/inventory --extra-vars "@group_vars/all.yaml" k3s/reset.yaml;;
-        [6]* ) echo "Rebooting nut server"
+        [fF]* ) echo "Deleting k3s cluster"
+               ansible-playbook -i inventory/inventory --extra-vars "@group_vars/all.yaml" k3s/reset.yml;;
+        [gG]* ) echo "Rebooting nut server"
                ansible-playbook -i inventory/inventory --extra-vars "@group_vars/all.yaml" server/server-reboot-nut-server.yaml;;
-        [7]* ) echo "Updating servers"
+        [hH]* ) echo "Updating servers"
                ansible-playbook -i inventory/inventory --extra-vars "@group_vars/all.yaml" server/server-update-installed-packages.yaml;;
-
-		# No cluster setup
-		[8]* ) echo "Exiting!"
+        [iI]* ) echo "Setting up cluster pods"
+               ansible-playbook -i inventory/inventory --extra-vars "@group_vars/all.yaml" pods/setup-cluster-services.yaml;;
+        [jJ]* ) echo "Removing cluster pods"
+               ansible-playbook -i inventory/inventory --extra-vars "@group_vars/all.yaml" pods/remove-cluster-services.yaml;;
+		[kK]* ) echo "Exiting!"
 		       exit;;
 	esac
 done
